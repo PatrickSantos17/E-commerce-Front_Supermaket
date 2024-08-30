a5d9589810dd9c8b9404eead92162d0342ffef99
LUCAS PEREIRA ALVES DE GOUVEIA <lucas.pagouveia@senacsp.edu.br>
2024-08-29 22:22:18 -0300

    Conexão com o banco de dados para o cadastro de usuarios

---
+++ JS/CadastroUsuario/conexaoAPI.js
@@ -0,0 +1,51 @@
+document.querySelector("form").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const form = event.target;
+    if (form.checkValidity() && validarSenha() === true) {
+        const usuario = {
+            nome: form.querySelector("#nomeCompleto").value,
+            cpf: form.querySelector("#cpf").value,
+            email: form.querySelector("#email").value,
+            senha: form.querySelector('#senha').value,
+            grupo: form.querySelector('#grupo').value
+        };
+
+        cadastrar(usuario);
+        document.getElementById("form").addEventListener("click", function () {
+            removerInvalidFeedbackClass();
+        });
+        limparCampos();
+    }
+});
+
+function cadastrar(usuario) {
+    mostrarLoading();
+    fetch('http://localhost:8080/usuario/cadastro', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(usuario)
+    })
+        .then(response => {
+            if (response.status === 201) {
+                setTimeout(() => {
+                    esconderLoading();
+                    document.querySelector(".card").style.display = "flex";
+                }, 3000);
+            } else if (response.status === 409) {
+                alert("CPF/E-mail já cadastrado.");
+                document.querySelector(".main").classList.remove('blur');
+                document.querySelector("footer").classList.remove('blur');
+                esconderLoading();
+            }
+        })
+        .catch(error => {
+            console.error('Erro ao cadastrar usuário:', error);
+            alert("Erro ao cadastrar usuário. Por favor, tente novamente.");
+            esconderLoading();
+            document.querySelector(".main").classList.remove('blur');
+            document.querySelector("footer").classList.remove('blur');
+        });
+}
