8fdadd91abda6f2bb3f0680c7f4b13af322abc52
Lucas Gouveia <lucaspereiraa81@gmail.com>
2024-09-05 23:35:02 -0300

    Inclusão do node e cypress no projeto

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,19 @@
+describe('Login Test', () => {
+    it('should log in successfully with valid credentials', () => {
+        // Visita a página de login
+        cy.visit('TelaLogin.html'); // Altere a URL conforme necessário
+
+        // Insere o nome de usuário
+        cy.get('input[placeholder="Digite seu E-mail"]').type('lucas@teste.com'); // Altere o valor conforme necessário
+
+        // Insere a senha
+        cy.get('input[type="password"]').type('12345'); // Altere o seletor e o valor conforme necessário
+
+        // Clica no botão de login
+        cy.get('button[type="submit"]').click(); // Altere o seletor conforme necessário
+
+        // Verifica se o modal de sucesso aparece
+        cy.get('.cartao .modal-dados-pessoais .message').should('contain.text', 'Login Realizado com sucesso!');
+
+    });
+});
