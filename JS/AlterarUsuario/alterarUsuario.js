c41256ce88c71f583df8d715e5613740e1fba5ad
ANTONIO VALMIR CANTO SALGADO NETO <antoniovcsneto@gmail.com>
2024-09-07 11:16:30 -0300

    Ajuste alterar usuario

--- JS/AlterarUsuario/alterarUsuario.js
+++ JS/AlterarUsuario/alterarUsuario.js
@@ -1,3 +1,6 @@
+// var API = "4.228.231.177"; //Setar essa variavel quando subir para a nuvem e comentar a localhost
+var API = "localhost"; //Setar essa variavel quando testar local e comentar a do IP
+ 
 document.addEventListener('DOMContentLoaded', function() {
     const urlParams = new URLSearchParams(window.location.search);
     const usuarioId = urlParams.get('id');
@@ -5,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
         acessarUsuário(usuarioId);
         console.log(usuarioId);
     }
-
+ 
     function acessarUsuário(usuarioId) {
         fetch('http://'+API+':8080/usuario/consultar/' + usuarioId)
             .then(response => response.json())
@@ -20,12 +23,12 @@ document.addEventListener('DOMContentLoaded', function() {
                 alert("Erro ao acessar usuário. Por favor, tente novamente.");
             });
     }
-
+ 
     document.querySelector("form").addEventListener("submit", function (event) {
         event.preventDefault(); // Evita o envio padrão do formulário
-
+ 
         const form = event.target;
-
+ 
         // Verifica se o formulário é válido e se a senha é válida (se a senha for fornecida)
         if (form.checkValidity() && validarSenha()) {
             const usuario = {
@@ -35,16 +38,16 @@ document.addEventListener('DOMContentLoaded', function() {
                 senha: form.querySelector('#senha').value || "", // Permite senha vazia
                 grupo: form.querySelector('#grupo').value
             };
-
+ 
             if (typeof usuarioId !== 'undefined') {
                 // Abre o modal de confirmação
                 $('#alterarUsuarioModal').modal('show');
-
+ 
                 // Adiciona evento ao botão de confirmação do modal
                 document.getElementById('confirmAlterarUsuario').addEventListener('click', function() {
                     alterarUsuario(usuarioId, usuario);
                 });
-
+ 
                 // Adiciona evento ao botão de cancelamento do modal
                 document.querySelector('#alterarUsuarioModal .btn-secondary').addEventListener('click', function() {
                     $('#alterarUsuarioModal').modal('hide');
@@ -52,13 +55,13 @@ document.addEventListener('DOMContentLoaded', function() {
             } else {
                 console.error("ID do usuário não está definido.");
             }
-
+ 
             limparCampos(); // Limpa os campos após o envio
         } else {
             form.classList.add('was-validated'); // Adiciona feedback de validação
         }
     });
-
+ 
     function alterarUsuario(usuarioId, usuario) {
         fetch('http://'+API+':8080/usuario/alterar/' + usuarioId, {
             method: 'PUT',
@@ -80,16 +83,16 @@ document.addEventListener('DOMContentLoaded', function() {
             alert("Erro ao alterar usuário. Por favor, tente novamente.");
         });
     }
-
+ 
     function validarSenha() {
         var senha = document.getElementById("senha").value;
         var confirmSenha = document.getElementById("confirmSenha").value;
-
+ 
         // Permite senha vazia
         if (senha === "" && confirmSenha === "") {
             return true;
         }
-
+ 
         // Verifica o comprimento mínimo da senha
         if (senha.length >= 5) {
             document.getElementById("senha").classList.remove("is-invalid");
@@ -99,7 +102,7 @@ document.addEventListener('DOMContentLoaded', function() {
             document.querySelector("#senha + .invalid-feedback").textContent = "A senha deve ter pelo menos 5 caracteres.";
             return false;
         }
-
+ 
         // Verifica se as senhas coincidem
         if (senha.length >= 5 && senha === confirmSenha) {
             document.getElementById("confirmSenha").classList.remove("is-invalid");
@@ -112,14 +115,14 @@ document.addEventListener('DOMContentLoaded', function() {
             return false;
         }
     }
-
+ 
     document.getElementById("senha").addEventListener("input", validarSenha);
     document.getElementById("confirmSenha").addEventListener("input", validarSenha);
-
+ 
     // Validação de CPF
     document.getElementById("cpf").addEventListener("blur", function () {
         var cpf = this.value;
-
+ 
         if (!cpf.match(/^\d{11}$/)) {
             this.classList.add("is-invalid");
             this.nextElementSibling.textContent = "Digite um CPF válido contendo exatamente 11 dígitos.";
@@ -128,15 +131,14 @@ document.addEventListener('DOMContentLoaded', function() {
             this.nextElementSibling.textContent = "";
         }
     });
-
+ 
     document.getElementById("cpf").addEventListener("input", function () {
         this.value = this.value.replace(/\D/g, '');
-
+ 
         if (this.value.length > 11) {
             this.value = this.value.slice(0, 11);
         }
     });
-
     // Adiciona evento ao botão de cancelamento para redirecionar para a página de listagem de usuários
     document.querySelector('.btn-secondary').addEventListener('click', function() {
         window.location.href = 'TelaListagemUsuarios.html';
