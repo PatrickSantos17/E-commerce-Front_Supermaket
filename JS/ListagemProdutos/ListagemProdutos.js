c42267e9d5f5e7c39567ec26899264d630c3feaa
Lucas Gouveia <lucaspereiraa81@gmail.com>
2024-09-15 21:21:56 -0300

    Ajuste para atender os critérios da listagem para estoquista

--- JS/ListagemProdutos/ListagemProdutos.js
+++ JS/ListagemProdutos/ListagemProdutos.js
@@ -1,6 +1,8 @@
 // var API = "4.228.231.177"; //Setar essa variavel quando subir para a nuvem e comentar a localhost
 var API = "localhost"; //Setar essa variavel quando testar local e comentar a do IP
 
+var grupoUsuarioLogado = localStorage.getItem("grupo");
+
 const rowsPerPage = 10;
 let currentPage = 1;
 let totalPages = 0;
@@ -20,12 +22,34 @@ async function fetchData(page = 0) {
 }
 
 function displayTableData() {
-    const tableBody = document.getElementById('table-body');
-    tableBody.innerHTML = '';
+    const tabela = document.querySelector('.divTable');
 
-    data.forEach(item => {
-        const row = document.createElement('tr');
-        row.innerHTML = `
+    if (grupoUsuarioLogado === "Admin") {
+        tabela.innerHTML = `
+        <table>
+            <thead> 
+                <tr>       
+                    <th>Imagem</th>
+                    <th>Código</th>
+                    <th>Nome</th>
+                    <th>Quantidade</th>
+                    <th>Valor</th>
+                    <th>Status</th>
+                    <th class="acao">Alterar</th>
+                    <th class="acao">Visualizar</th>
+                    <th class="acao">Hab/Des</th>
+                </tr>
+            </thead>
+            <tbody id="table-body">
+            </tbody>
+        </table>
+                        `;
+        const tableBody = document.getElementById('table-body');
+        tableBody.innerHTML = '';
+
+        data.forEach(item => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
             <td><img src="${item.urlImagemPrincipal}" alt="${item.nomeProduto}" width="50"></td>
             <td>${item.id}</td>
             <td>${item.nomeProduto}</td>
@@ -36,8 +60,45 @@ function displayTableData() {
             <td class="acao"><button>Alterar</button></td>
             <td class="acao"><button>Hab/Des</button></td>
         `;
-        tableBody.appendChild(row);
-    });
+            tableBody.appendChild(row);
+        });
+    } else if (grupoUsuarioLogado === "Estoquista") {
+        tabela.innerHTML = `
+        <table>
+            <thead> 
+                <tr>       
+                    <th>Imagem</th>
+                    <th>Código</th>
+                    <th>Nome</th>
+                    <th>Quantidade</th>
+                    <th>Valor</th>
+                    <th>Status</th>
+                    <th class="acao">Alterar</th>
+                </tr>
+            </thead>
+            <tbody id="table-body">
+            </tbody>
+        </table>
+                        `;
+        const tableBody = document.getElementById('table-body');
+        tableBody.innerHTML = '';
+        data.forEach(item => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+            <td><img src="${item.urlImagemPrincipal}" alt="${item.nomeProduto}" width="50"></td>
+            <td>${item.id}</td>
+            <td>${item.nomeProduto}</td>
+            <td>${item.quantidade}</td>
+            <td>${item.preco}</td>
+            <td>${item.ativo ? 'Ativo' : 'Inativo'}</td>
+            <td class="acao"><button>Alterar</button></td>
+        `;
+            tableBody.appendChild(row);
+        });
+    } else {
+        alert("Você não tem permissão para acessar esta página!");
+        window.location.href = 'TelaLogin.html';
+    }
 }
 
 function setupPagination() {
