5df06de9ecf24fbd75326482af7429cd9b94554f
Patrick Santos17 <pstricksantoss@gmail.com>
2024-10-06 01:09:17 -0300

    Excluindo produtos do carrinho

--- JS/Produto/Produto.js
+++ JS/Produto/Produto.js
@@ -43,20 +43,55 @@ function montarLayoutExibicao(produtos) {
             const estrelasCheias = Math.floor(avaliacao); // Número de estrelas cheias
             const temEstrelaMeia = (avaliacao % 1) >= 0.5; // Verifica se tem estrela meia
 
-            // Adiciona estrelas cheias
+            // Adiciona estrelas cheias (SVG)
             for (let i = 0; i < estrelasCheias; i++) {
-                estrelasHTML += '<span class="star filled">★</span>'; // Estrela cheia
+                estrelasHTML += `
+                    <svg height="24px" width="24px" class="star filled" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <g><g><path d="M9.362,9.158c0,0-3.16,0.35-5.268,0.584c-0.19,0.023-0.358,0.15-0.421,0.343s0,0.394,0.14,0.521
+                            c1.566,1.429,3.919,3.569,3.919,3.569c-0.002,0-0.646,3.113-1.074,5.19c-0.036,0.188,0.032,0.387,0.196,0.506
+                            c0.163,0.119,0.373,0.121,0.538,0.028c1.844-1.048,4.606-2.624,4.606-2.624s2.763,1.576,4.604,2.625
+                            c0.168,0.092,0.378,0.09,0.541-0.029c0.164-0.119,0.232-0.318,0.195-0.505c-0.428-2.078-1.071-5.191-1.071-5.191
+                            s2.353-2.14,3.919-3.566c0.14-0.131,0.202-0.332,0.14-0.524s-0.23-0.319-0.42-0.341c-2.108-0.236-5.269-0.586-5.269-0.586
+                            s-1.31-2.898-2.183-4.83c-0.082-0.173-0.254-0.294-0.456-0.294s-0.375,0.122-0.453,0.294C10.671,6.26,9.362,9.158,9.362,9.158z"></path></g></g>
+                    </svg>`;
             }
 
-            // Adiciona estrela meia se necessário
+            // Adiciona estrela meia se necessário (SVG)
             if (temEstrelaMeia) {
-                estrelasHTML += '<span class="star half">★</span>'; // Estrela meia
+                estrelasHTML += `
+                    <svg height="24px" width="24px" class="star half" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <defs>
+                            <linearGradient id="half-fill" x1="0%" y1="0%" x2="100%" y2="0%">
+                                <stop offset="50%" style="stop-color:#ffeb49;stop-opacity:1" />
+                                <stop offset="50%" style="stop-color:#666;stop-opacity:1" />
+                            </linearGradient>
+                        </defs>
+                        <g>
+                            <path d="M9.362,9.158c0,0-3.16,0.35-5.268,0.584c-0.19,0.023-0.358,0.15-0.421,0.343s0,0.394,0.14,0.521
+                                c1.566,1.429,3.919,3.569,3.919,3.569c-0.002,0-0.646,3.113-1.074,5.19c-0.036,0.188,0.032,0.387,0.196,0.506
+                                c0.163,0.119,0.373,0.121,0.538,0.028c1.844-1.048,4.606-2.624,4.606-2.624s2.763,1.576,4.604,2.625
+                                c0.168,0.092,0.378,0.09,0.541-0.029c0.164-0.119,0.232-0.318,0.195-0.505c-0.428-2.078-1.071-5.191-1.071-5.191
+                                s2.353-2.14,3.919-3.566c0.14-0.131,0.202-0.332,0.14-0.524s-0.23-0.319-0.42-0.341c-2.108-0.236-5.269-0.586-5.269-0.586
+                                s-1.31-2.898-2.183-4.83c-0.082-0.173-0.254-0.294-0.456-0.294s-0.375,0.122-0.453,0.294C10.671,6.26,9.362,9.158,9.362,9.158z">
+                            </path>
+                        </g>
+                    </svg>
+                `;
             }
+            
 
-            // Adiciona estrelas vazias
+            // Adiciona estrelas vazias (SVG)
             const estrelasVazias = 5 - estrelasCheias - (temEstrelaMeia ? 1 : 0);
             for (let i = 0; i < estrelasVazias; i++) {
-                estrelasHTML += '<span class="star">☆</span>'; // Estrela vazia
+                estrelasHTML += `
+                    <svg height="24px" width="24px" class="star empty" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <g><g><path d="M9.362,9.158c0,0-3.16,0.35-5.268,0.584c-0.19,0.023-0.358,0.15-0.421,0.343s0,0.394,0.14,0.521
+                            c1.566,1.429,3.919,3.569,3.919,3.569c-0.002,0-0.646,3.113-1.074,5.19c-0.036,0.188,0.032,0.387,0.196,0.506
+                            c0.163,0.119,0.373,0.121,0.538,0.028c1.844-1.048,4.606-2.624,4.606-2.624s2.763,1.576,4.604,2.625
+                            c0.168,0.092,0.378,0.09,0.541-0.029c0.164-0.119,0.232-0.318,0.195-0.505c-0.428-2.078-1.071-5.191-1.071-5.191
+                            s2.353-2.14,3.919-3.566c0.14-0.131,0.202-0.332,0.14-0.524s-0.23-0.319-0.42-0.341c-2.108-0.236-5.269-0.586-5.269-0.586
+                            s-1.31-2.898-2.183-4.83c-0.082-0.173-0.254-0.294-0.456-0.294s-0.375,0.122-0.453,0.294C10.671,6.26,9.362,9.158,9.362,9.158z"></path></g></g>
+                    </svg>`;
             }
 
             produtosHTML += `
@@ -81,6 +116,7 @@ function montarLayoutExibicao(produtos) {
 
     listaProdutos.innerHTML = produtosHTML;
 }
+
 function formatarCasasDecimais(numero) {
     return Number(numero).toFixed(2);
 }
