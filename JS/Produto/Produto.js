7c6e6630317da743a1a579a0d8cf7a2c3ef703cf
Patrick Santos17 <pstricksantoss@gmail.com>
2024-10-07 01:14:49 -0300

    Ajuste de design tela de produtos, visão cliente

--- JS/Produto/Produto.js
+++ JS/Produto/Produto.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 async function fetchProduto() {
     try {
-        const response = await fetch(`http://localhost:8080/produto/listagem`, {
+        const response = await fetch(`http://localhost:8080/produto/listagemAtivos`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
@@ -104,7 +104,7 @@ function montarLayoutExibicao(produtos) {
                                 <hr>
                             </div>
                             <p class="card-text"><strong>R$ ${formatarCasasDecimais(produto.preco)}</strong></p>
-                            <p>Avaliação: ${estrelasHTML}</p> <!-- Aqui estão as estrelas -->
+                            <p> ${estrelasHTML}</p> <!-- Aqui estão as estrelas -->
                             <a href="TelaDetalheProduto.html?produtoId=${produto.id}" class="btn btn-primary">Detalhes do produto</a>
                             <button onclick="adicionarCarrinho(${produto.id})" class="btn btn-primary">Comprar</button>
                         </div>
