d64f94d631750949ebe1e3b81b1f4e5fe2e8f527
Lucas Gouveia <lucaspereiraa81@gmail.com>
2024-10-20 21:22:52 -0300

    Criação modal confirmação logout

--- JS/HeaderCliente/HeaderCliente.js
+++ JS/HeaderCliente/HeaderCliente.js
@@ -24,19 +24,63 @@ if (localStorage.getItem("produtos")) {
     qtdCarrinho.innerHTML = listaProdutos.length;
 }
 
-// Função de logout
-logoutButton.addEventListener('click', function () {
-    // localStorage.removeItem('nome');   // Remove o nome do usuário
-    // localStorage.removeItem('grupo');  // Remove o grupo (Admin ou cliente)
-    localStorage.clear();
-    window.location.href = "TelaProduto.html";  // Redireciona para a tela de login
+logoutButton.addEventListener('click', function (event) {
+    event.preventDefault();
 });
 
 document.querySelector('.dados').addEventListener('click', function (event) {
-    event.preventDefault(); 
+    event.preventDefault();
     let autenticado = localStorage.getItem("autenticado");
     const isAutenticado = (autenticado.toLowerCase() === "true")
     if (isAutenticado === true) {
         window.location.href = 'TelaAlterarCliente.html?userId=' + localStorage.getItem("id");
     }
 });
+
+function abrirModalConfirmacao() {
+    document.querySelector('.card-confirmar').innerHTML = `
+                    <div class="card-content">
+                        <p class="card-heading"></p>
+                        <p class="card-description">Deseja realmente sair?</p>
+                    </div>
+                    <div class="card-button-wrapper">
+                        <button onclick="fecharModalConfirmacao()" class="card-button secondary">Cancelar</button>
+                        <button class="card-button primary confirmar-saida">Sair</button>
+                    </div>
+                    <button onclick="fecharModalConfirmacao()" class="exit-button">
+                        <svg height="20px" viewBox="0 0 384 512">
+                            <path
+                                d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z">
+                            </path>
+                        </svg>
+                    </button>
+    
+    `;
+    document.getElementById("overlay").style.display = "flex";
+    document.querySelector('main').classList.add("blur");
+    document.querySelector("header").classList.add("blur");
+    document.querySelector('.card-confirmar').style.display = 'flex'; // Garante que o modal apareça
+
+    const modal = document.querySelector(".card-confirmar");
+    const heading = document.querySelector(".card-heading");
+    const btnConfirmar = modal.querySelector('.card-button.primary');
+
+    document.querySelector('.confirmar-saida').addEventListener('click', function (event) {
+        localStorage.removeItem("autenticado");
+        localStorage.removeItem("id");
+        localStorage.removeItem("nome");
+        localStorage.removeItem("grupo");
+        localStorage.removeItem("ativo");
+        window.location.href = 'TelaProduto.html';
+    });
+    btnConfirmar.onclick = () => {
+        fecharModalConfirmacao();
+    }
+}
+
+function fecharModalConfirmacao() {
+    document.querySelector(".card-confirmar").style.display = "none";
+    document.getElementById("overlay").style.display = "none";
+    document.querySelector("main").classList.remove("blur");
+    document.querySelector("header").classList.remove("blur");
+}
