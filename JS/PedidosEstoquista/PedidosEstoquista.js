1edaf2b9f676231f04a17fccaad96ab422c78593
Lucas Gouveia <lucaspereiraa81@gmail.com>
2024-11-21 00:27:06 -0300

    Ajustes alterar status pedido

--- JS/PedidosEstoquista/PedidosEstoquista.js
+++ JS/PedidosEstoquista/PedidosEstoquista.js
@@ -34,7 +34,7 @@ async function carregarPedidos() {
                 <td>${dataPedidoFormatada}</td>
                 <td>${pedido.status || "Status não disponível"}</td>
                 <td>R$ ${(pedido.total).toFixed(2) || "Valor não disponível"}</td>
-                <td class="text-center"><button onclick="alterarStatusPedido(${pedido.id})">Editar pedido</button></td>
+                <td class="text-center"><button onclick="openModalStatusPedido('${JSON.stringify(pedido).replace(/'/g, "\\'").replace(/"/g, '&quot;')}')">Editar pedido</button></td>
             `;
                 tbody.appendChild(row);
             });
@@ -49,13 +49,57 @@ async function carregarPedidos() {
     }
 }
 
-function alterarStatusPedido(pedidoId) {
-
+function alterarStatusPedido(alterarPedidoDTO) {
+    fetch(`http://${API}:8080/pedido/alterarStatus`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(alterarPedidoDTO)
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erro ao alterar status: ' + response.statusText);
+            }
+            document.querySelector(".modal-overlay").style.display = "flex";
+        })
+        .catch(error => {
+            console.error('Erro ao alterar cliente:', error);
+            alert('Erro ao alterar cliente. Por favor, tente novamente.');
+        });
 }
 
-function fecharModal() {
-    document.getElementById('detalhesModal').style.display = 'none';
+function openModalStatusPedido(pedidoString) {
+    const pedido = JSON.parse(pedidoString);
+    document.getElementById('cartao-alterarStatus').style.display = 'flex';
+    document.querySelector('.nPedido').value = pedido.id;
+    document.querySelector('.valor').value = "R$: " + (pedido.total).toFixed(2) || "Valor não disponível";
+    document.querySelector('#status').value = pedido.status || "Status não disponível";
+    document.querySelector('.cancelar').addEventListener('click', (event) => {
+        event.preventDefault();
+        closeModalStatusPedido();
+    });
+    document.querySelector('.close').addEventListener('click', (event) => {
+        event.preventDefault();
+        closeModalStatusPedido();
+    });
+    document.querySelector('.alterar').addEventListener('click', (event) => {
+        event.preventDefault();
+        const alterarPedidoDTO = {
+            idPedido: pedido.id,
+            status: document.querySelector('#status').value
+        };
+        alterarStatusPedido(alterarPedidoDTO);
+    });
 }
+
+function closeModalStatusPedido() {
+    document.getElementById('cartao-alterarStatus').style.display = 'none';
+}
+
+document.querySelector('.accept-cookie-button').addEventListener('click', function (event) {
+    location.reload();
+});
 
 // Executa a função ao carregar a página
 document.addEventListener('DOMContentLoaded', carregarPedidos);
